9be6c36c575679ed0705427530bedb4c713beb1a
Denilson Mucavele <denilsonmucavel@outlook.com>
2021-12-18 00:50:58 +0200

    avoinding the duplication rules

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -3,12 +3,17 @@ import { Category } from "../entities/Category";
 
 type CategoryRequest  = {
     name: string;
-    description:string;
+    description: string;
 }
 
 export class CreateCategoryService{
-    async execute({name, description}:CategoryRequest): Promise<Category>{
+    async execute({name, description}:CategoryRequest): Promise<Category | Error>{
         const repo = getRepository(Category);
+
+        //select * from categories where name = "name" limit 1
+        if (await repo.findOne({ name })) {
+            return new Error("Category already exists");
+        }
 
         const category = repo.create({
             name,
